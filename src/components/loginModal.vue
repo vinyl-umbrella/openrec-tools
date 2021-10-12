0dc97691f1bcf5c4309d4311c350fec7bf425f09
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2021-10-12 21:43:35 +0900

    add getUserInfo

--- src/components/loginModal.vue
+++ src/components/loginModal.vue
@@ -28,6 +28,9 @@
         >
       </div>
       <div v-else>
+        <img :src="userInfo.icon" style="border-radius: 50%" />
+        <div>{{ userInfo.nickname }}({{ userInfo.userid }})</div>
+        <br />
         <v-btn
           @click="orLogout()"
           color="var(--v-background-lighten1)"
@@ -54,19 +57,51 @@ export default {
     return {
       orAccessToken: "",
       orUuid: "",
+      userInfo: {
+        nickname: "",
+        userid: "",
+        icon: ""
+      },
     };
   },
+
+  mounted() {
+    this.getUserInfo();
+  },
+
   methods: {
     orLogin() {
       localStorage.setItem("orAccessToken", this.orAccessToken);
       localStorage.setItem("orUuid", this.orUuid);
       this.$emit("updateLoginStatus", true);
+      this.getUserInfo();
     },
 
     orLogout() {
       localStorage.removeItem("orAccessToken");
       localStorage.removeItem("orUuid");
       this.$emit("updateLoginStatus", false);
+    },
+
+    async getUserInfo() {
+      if (localStorage.getItem("orAccessToken") && localStorage.getItem("orUuid")) {
+        let param = {
+          method: "GET",
+          headers: {
+            Accept: "application/json,text/plain,*/*",
+            uuid: localStorage.getItem("orUuid"),
+            "access-token": localStorage.getItem("orAccessToken"),
+          },
+        };
+        let res = await fetch("https://apiv5.openrec.tv/api/v5/users/me", param);
+        if (res.ok) {
+          let j = await res.json();
+          j = j.data.items[0];
+          this.userInfo.nickname = j.nickname;
+          this.userInfo.userid = j.id;
+          this.userInfo.icon = j.icon_image_url;
+        }
+      }
     },
   },
 };
