320a6a40cce9c752ba81657c10a4d80b906250f4
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2021-11-04 22:05:12 +0900

    remove source map

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 
 module.exports = {
+    productionSourceMap: false,
     configureWebpack: {
         plugins: [
             new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /ja/),
