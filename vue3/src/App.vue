bc1a2ef7a78ed073a32fb576f5bc54f4c85af160
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2022-01-24 23:40:03 +0900

    use primevue

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,30 +1,44 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <div id="nav">
+      <Button @click="menu = true" icon="pi pi-list" />
+      <Sidebar :visible="menu" :base-z-index="1000">
+        <h3>Left Sidebar</h3>
+        <router-link to="/">Home</router-link>
+        <br />
+        <router-link to="/about">About</router-link>
+      </Sidebar>
+    </div>
+    <router-view />
   </div>
-  <router-view />
 </template>
 
+<script setup>
+import { ref } from "vue";
+
+const menu = ref(false);
+</script>
+
 <style>
+body {
+  margin: 0;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  background-color: var(--surface-a);
+  font-weight: 400;
+  color: var(--text-color);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
   padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  text-align: left;
 }
 </style>
--- vue3/src/main.js
+++ vue3/src/main.js
@@ -1,6 +1,25 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import App from "@/App.vue";
+import router from "@/router";
 
-createApp(App).use(store).use(router).mount("#app");
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import Toast from "primevue/toast";
+import ToastService from "primevue/toastservice";
+import Sidebar from "primevue/sidebar";
+
+// import "primevue/resources/themes/arya-blue/theme.css";
+import "@/style/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+const app = createApp(App);
+app.use(router);
+app.use(PrimeVue);
+app.use(ToastService);
+
+app.component("Button", Button);
+app.component("Toast", Toast);
+app.component("Sidebar", Sidebar);
+
+app.mount("#app");
