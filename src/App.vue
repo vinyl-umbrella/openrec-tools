fe381d202c0b679565bc958d6f9f402e350b2bca
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2022-02-03 11:56:04 +0900

    lint

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,16 @@
     <Sidebar v-model:visible="menu">
       <h3 style="margin: 0">Menu</h3>
       <router-link to="/" @click="menu = false">Masaoroid</router-link><br />
-      <router-link to="rank" @click="menu = false">ランキング</router-link><br />
-      <router-link to="/message" @click="menu = false">過去ログ検索</router-link><br />
-      <router-link to="/chat" @click="menu = false">コメビュ</router-link><br />
-      <router-link to="/stream" @click="menu = false">コメつき配信</router-link><br />
-      <router-link to="/api" @click="menu = false">OPENREC API</router-link><br />
+      <router-link to="rank" @click="menu = false">ランキング</router-link
+      ><br />
+      <router-link to="/message" @click="menu = false">過去ログ検索</router-link
+      ><br />
+      <router-link to="/chat" @click="menu = false">コメビュ</router-link>
+      <br />
+      <router-link to="/stream" @click="menu = false">コメつき配信</router-link>
+      <br />
+      <router-link to="/api" @click="menu = false">OPENREC API</router-link>
+      <br />
       <router-link to="/contact" @click="menu = false">問い合わせ</router-link>
     </Sidebar>
   </div>
