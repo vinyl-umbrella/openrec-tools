ab7749cc842ae31c3e803ff7fbc792ec6db14df3
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2022-05-15 20:58:58 +0900

    add test

--- functions/__test__/msg.test.js
+++ functions/__test__/msg.test.js
@@ -11,8 +11,22 @@ describe('msg test', () => {
             videoid: 'ov82d410wzw',
             search_string: 'クソガキ',
             startdate: '',
-            enddate: '',
             border: 0
+        }
+        let res = await request(app.api).post('/v1/messages').send(data);
+        expect(res.statusCode).toBe(200);
+        for (let b of res.body) {
+            expect(b.id).toBeDefined();
+            expect(b.time).toBeDefined();
+            expect(b.userid).toBeDefined();
+            expect(b.message).toBeDefined();
+            expect(b.notexists).not.toBeDefined();
+        }
+    })
+
+    it('msg only id', async () => {
+        const data = {
+            videoid: 'ov82d410wzw',
         }
         let res = await request(app.api).post('/v1/messages').send(data);
         expect(res.statusCode).toBe(200);
